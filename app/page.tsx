ccfc0a581cdbb87e49fbec24cc9e14596141d53e
Michael <michael.boitmann@gmail.com>
2024-06-25 23:29:05 +1200

    added components function

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
 "use client";
 
 import { Tldraw } from "@tldraw/tldraw"
+import GenerateButton from './../components/GenerateButton';
 
 export default function Home() {
   return (
-    <main>
-      <Tldraw />
+    <main className="h-screen w-screen">
+      <Tldraw persistenceKey="snapsite">
+        <GenerateButton />
+      </Tldraw>
     </main>
   )
 }
--- components/GenerateButton.tsx
+++ components/GenerateButton.tsx
@@ -1,3 +1,7 @@
 export default function GenerateButton() {
-  return "generate button";
+  return (
+    <button className="bg-blue-500 text-white font-semibold rounded-lg text-lg z-[1000] absolute top-4 left-1/2 -translate-x-1/2 py-2 px-4 shadow-md shadow-blue-800/50 hover:bg-blue-600">
+      Generate
+    </button>
+  )
 }
