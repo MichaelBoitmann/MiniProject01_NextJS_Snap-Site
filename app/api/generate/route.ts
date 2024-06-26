8a19ff7887043750070f230cb5815dba2f1a103f
Michael <michael.boitmann@gmail.com>
2024-06-26 22:08:04 +1200

    worked on route.ts

--- app/api/generate/route.ts
+++ app/api/generate/route.ts
@@ -4,9 +4,37 @@ new OpenAi({
   apiKey: process.env.OPENAI_API_KEY
 })
 
+const systemPrompt = `You are an expert TailwindCSS developer. 
+A user will be provide you with a low-fidelity wireframe of an
+application and you will return a single HTML file that uses
+TailwindCSS to create the website. Use of creative license to 
+make the applicaton more complete. If you need to insert an image,
+use the service placehold to create a placeholder image. Respond
+only with the HTML file.`
 
-export async function POST() {
-  await openai.chat.completions.create({
-    
-  })
+
+export async function POST(request: Request) {
+  const{ image } = await request.jsonOpenAi()
+
+  if (!image) {
+    return NextResponse.json("No image provided", {
+      status: 400,
+    })
+  }
+
+  try {
+    const completion = await openai.chat.completions.create({
+      model: "gpt-4-vision-preview",
+      max_tokens: 4096,
+      //provide a prompt to "system" (instructions)
+      messages: [
+        { role: "system", content: systemPrompt },
+        { role: "user", content: [{ type: "image_url", image_url: image }] },
+      ],
+    })
+
+    return NextResponse.json(completion)
+    } catch (error) {
+      return NextResponse.json("Internal server error", { status: 500 });
+    }
 }
